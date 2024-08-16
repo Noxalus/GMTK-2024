5df69a6506a66a5621d0680340894d0c773a9315
Noxalus <noxalus@gmail.com>
2024-08-16 22:53:31 +0200

    Plug implemented animation for movement, attack and jump.

--- gmtk-2024/scripts/player.gd
+++ gmtk-2024/scripts/player.gd
@@ -13,13 +13,13 @@ class_name Player
 @onready var anim_tree = $AnimationTree
 @onready var anim_state = $AnimationTree.get("parameters/playback")
 
-var gravity = get_gravity()
 var jumping = false
+var last_floor = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += gravity * delta
+		velocity += get_gravity() * delta
 
 	get_move_input(delta)
 	move_and_slide()
@@ -27,26 +27,28 @@ func _physics_process(delta):
 	if (velocity.length() > 1.0):
 		model.rotation.y = lerp_angle(model.rotation.y, spring_arm.rotation.y + 135, rotation_speed * delta)
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-		#velocity.y = jump_speed
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_speed
+		jumping = true
+		anim_tree.set("parameters/conditions/jumping", true)
+		anim_tree.set("parameters/conditions/grounded", false)
+	if is_on_floor() and not last_floor:
+		jumping = false
+		anim_tree.set("parameters/conditions/jumping", false)
+		anim_tree.set("parameters/conditions/grounded", true)
+	if not is_on_floor() and not jumping:
+		anim_state.travel("Jump_Idle")
+		anim_tree.set("parameters/conditions/grounded", false)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	#if direction:
-		#velocity.x = direction.x * speed
-		#velocity.z = direction.z * speed
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, speed)
-		#velocity.z = move_toward(velocity.z, 0, speed)
+	last_floor = is_on_floor()
 
 func get_move_input(delta):
 	var vy = velocity.y
 	velocity.y = 0
 	var input = Input.get_vector("left", "right", "forward", "back")
 	var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)
+	var vl = velocity * model.transform.basis
+	anim_tree.set("parameters/IdleWalkRun/blend_position", Vector2(-vl.x, vl.z) / speed)
 	velocity = lerp(velocity, dir * speed, acceleration * delta)
 	velocity.y = vy
 	
@@ -61,3 +63,5 @@ func _unhandled_input(event):
 		spring_arm.rotation.x -= event.relative.y * mouse_sensitivity
 		spring_arm.rotation_degrees.x = clamp(spring_arm.rotation_degrees.x, -90.0, 30.0)
 		spring_arm.rotation.y -= event.relative.x * mouse_sensitivity
+	if event.is_action_pressed("attack"):
+		anim_state.travel("NoWeaponAttack")
