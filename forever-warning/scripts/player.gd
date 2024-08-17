3b0563671e81b0bdc186038558bc0302558b8d32
Noxalus <noxalus@gmail.com>
2024-08-17 11:37:08 +0200

    Make the player rotate with the mouse position / right gamepad stick.

--- forever-warning/scripts/player.gd
+++ forever-warning/scripts/player.gd
@@ -2,14 +2,19 @@ extends Area2D
 
 @export var speed: float = 500
 @export var focus_factor: float = 0.5
+@export var rotation_speed: float = 1
+
+@onready var sprite = $Sprite2D
 
 var vel := Vector2(0, 0)
 var cur_speed := speed
+var previous_mouse_position
 
 func _process(delta):
 	pass
 
 func _physics_process(delta):
+	# Update position
 	var dirVel := Vector2(0, 0)
 	
 	if Input.is_action_pressed("move_up"):
@@ -29,3 +34,17 @@ func _physics_process(delta):
 
 	vel = dirVel.normalized() * cur_speed;
 	position += vel * delta;
+	
+	# Update look at
+	
+	var gamepad_input = Input.get_vector("look_down", "look_up", "look_left", "look_right")
+	var mouse_position = get_global_mouse_position()
+	
+	if gamepad_input.length() > 0.0:
+		sprite.rotation = gamepad_input.angle()
+	elif previous_mouse_position != mouse_position:
+		sprite.look_at(mouse_position)
+		sprite.rotate(PI / 2.0)
+	
+	previous_mouse_position = mouse_position
+	
