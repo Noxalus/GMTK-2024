6599651952f1f0481c83fbc5d4bc5e418d6efd5d
Noxalus <noxalus@gmail.com>
2024-08-17 12:42:24 +0200

    Start to add the boss core with bullet collision and life.

--- forever-warning/scripts/bullet.gd
+++ forever-warning/scripts/bullet.gd
@@ -14,3 +14,8 @@ func set_direction(dir: Vector2):
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func _on_area_entered(area):
+	if area.is_in_group("damageable"):
+		area.damage(1)
+		queue_free()
